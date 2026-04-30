811d8a4a04df67003924e60887d7e9bf5da4bbac
Cong Pham <congpham2595@gmail.com>
2026-04-30 08:32:50 +0700

    #implement redis lock overdue task job

--- src/main/java/com/congpv/springboot_base_project/infrastructure/job/OverdueTaskJob.java
+++ src/main/java/com/congpv/springboot_base_project/infrastructure/job/OverdueTaskJob.java
@@ -7,12 +7,15 @@ import com.congpv.springboot_base_project.core.service.TaskService;
 import com.congpv.springboot_base_project.shared.dto.task.TaskResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
@@ -20,8 +23,9 @@ import java.util.List;
 public class OverdueTaskJob {
 
     private final TaskService taskService;
+    private final RedissonClient redissonClient;
 
-    // private final EmailService emailService;
+
     /**
      * Cấu hình Cron: Giây - Phút - Giờ - Ngày trong tháng - Tháng - Ngày trong tuần
      * "0 0 0 * * ?" -> Chạy vào đúng 00:00:00 (nửa đêm) mỗi ngày
@@ -30,24 +34,46 @@ public class OverdueTaskJob {
     @Scheduled(cron = "0 * * * * ?")
     @Transactional(readOnly = true)
     public void scanAndNotifyOverdueTasks() {
-        log.info("--- BẮT ĐẦU CRON JOB QUÉT TASK QUÁ HẠN ---");
+        String lockKey = "cron_job_overdue_tasks";
+        RLock lock = redissonClient.getLock(lockKey);
+        boolean isLocked = false;
 
-        LocalDateTime now = LocalDateTime.now();
-        List<TaskResponseDto> overdueTasks = taskService.getOverdueTasks(now.toLocalDate());
+        try {
+            isLocked = lock.tryLock(5, -1, TimeUnit.SECONDS);
+            if (isLocked) {
+                log.info("--- BẮT ĐẦU CRON JOB QUÉT TASK QUÁ HẠN ---");
 
-        if (overdueTasks.isEmpty()) {
-            log.info("Không có task nào quá hạn tại thời điểm này.");
-            return;
+                LocalDateTime now = LocalDateTime.now();
+                List<TaskResponseDto> overdueTasks = taskService.getOverdueTasks(now.toLocalDate());
+
+                if (overdueTasks.isEmpty()) {
+                    log.info("Không có task nào quá hạn tại thời điểm này.");
+                    return;
+                }
+
+                log.warn("CẢNH BÁO: Phát hiện {} task đã quá hạn!", overdueTasks.size());
+
+                for (TaskResponseDto task : overdueTasks) {
+                    log.info("Xử lý Task ID: {} - Title: {} - Người chịu trách nhiệm: {}",
+                            task.id(), task.title(),
+                            task.assigneeUsername() != null ? task.assigneeUsername() : "Chưa gán");
+                }
+
+                log.info("--- KẾT THÚC CRON JOB ---");
+            } else {
+                log.warn("⚠️ Another server is currently processing Task {}. Aborting current operation.", lockKey);
+            }
+
+        } catch (InterruptedException e) {
+            log.error("❌ Thread interrupted while waiting to acquire Redis lock", e);
+            Thread.currentThread().interrupt(); // Restore interrupted status
+
+        } finally {
+            if (isLocked && lock.isLocked() && lock.isHeldByCurrentThread()) {
+                lock.unlock();
+                log.info("🔓 Successfully released lock for Task {}.", lockKey);
+            }
         }
 
-        log.warn("CẢNH BÁO: Phát hiện {} task đã quá hạn!", overdueTasks.size());
-
-        for (TaskResponseDto task : overdueTasks) {
-            log.info("Xử lý Task ID: {} - Title: {} - Người chịu trách nhiệm: {}",
-                    task.id(), task.title(),
-                    task.assigneeUsername() != null ? task.assigneeUsername() : "Chưa gán");
-        }
-
-        log.info("--- KẾT THÚC CRON JOB ---");
     }
 }
